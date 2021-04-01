4a9986706a7ac9cc8b3f36b7367cbe0c3e87f3f0
MsVoxxie <odysseylove222@gmail.com>
2021-04-01 00:59:20 +0000

    Re-added proper git repo

--- Commands/Other/source.js
+++ Commands/Other/source.js
@@ -11,7 +11,7 @@ module.exports = {
         const embed = new MessageEmbed()
             .setTitle("__**ChiBot's Code**__")
             .setColor("#700707")
-            .setThumbnail("http://share.voxxie.me/i/OCVr2M.png")
+            .setThumbnail("https://share.voxxie.me/images/VoxelIco.png")
             .setDescription("Source Code can be found [Here on Github](https://github.com/MsVoxxie/ChiBot)\nMy Creator is 👑 Ms.Voxxie#0001\nCreate an Issue if anything comes up!")
 
         message.channel.send({ embed: embed });
