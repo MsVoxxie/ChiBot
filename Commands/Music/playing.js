abcae05384f998b642b7a0024f647458cf1122a8
MsVoxxie <odysseylove222@gmail.com>
2021-04-22 22:34:59 -0400

    Music Embed Fixes

--- Commands/Music/playing.js
+++ Commands/Music/playing.js
@@ -1,25 +1,26 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: "playing",
-    aliases: ["nowplaying", "np"],
-    description: "Check whats playing.",
-    category: "Music",
-    botPerms: ["CONNECT", "SPEAK", "MANAGE_MESSAGES"],
-    async execute(bot, message, args, settings) {
+	name: 'playing',
+	aliases: ['nowplaying', 'np'],
+	description: 'Check whats playing.',
+	category: 'Music',
+	botPerms: ['CONNECT', 'SPEAK', 'MANAGE_MESSAGES'],
+	async execute(bot, message, args, settings) {
 
-        const queue = bot.queue.get(message.guild.id);
-        if (!queue) return message.reply("There is nothing playing.").then(s => s.delete({ timeout: 30 * 1000 }));
-        const song = queue.songs[0];
+		const queue = bot.queue.get(message.guild.id);
+		if (!queue) return message.reply('There is nothing playing.').then(s => s.delete({ timeout: 30 * 1000 }));
+		const song = queue.songs[0];
 
-        let nowPlaying = new MessageEmbed()
-            .setTitle("Now playing")
-            .setDescription(`${song.title}\n${song.url}`)
-            .setColor(settings.color)
-            .setAuthor(bot.user.tag);
+		const nowPlaying = new MessageEmbed()
+			.setAuthor(song.requester.user.displayAvatarURL({ dynamic: true }), `${song.requester.nickname ? song.requester.nickname : ` ${song.requester.nickname} | ${song.requester.user.tag}`}`)
+			.setTitle('Now playing')
+			.setThumbnail(song.thumbnail)
+			.setDescription(`${song.title}\n${song.url}`)
+			.setColor(settings.color);
 
-        if (song.duration > 0) nowPlaying.setFooter(new Date(song.duration * 1000).toISOString().substr(11, 8));
+		if (song.duration > 0) nowPlaying.setFooter(new Date(song.duration * 1000).toISOString().substr(11, 8));
 
-        return message.channel.send(nowPlaying).then(s => s.delete({ timeout: 60 * 60000 }));
-    }
+		return message.channel.send(nowPlaying).then(s => s.delete({ timeout: 60 * 60000 }));
+	},
 };
